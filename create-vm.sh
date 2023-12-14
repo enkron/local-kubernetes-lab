b68aa47017cf3bfbb9de906c6af885b0cfdb3d1c
Sergei Belokon <srj.belokon@gmail.com>
2023-12-14 14:34:29 +1100

    fix: gave different names to ISO images
    `genisoimage` command doesn't overwrite cidata.iso created after 1st
    iteration hence every VM gets the same meta data (hostname/instance id)

--- create-vm.sh
+++ create-vm.sh
@@ -12,14 +12,14 @@ test -s $BASE_IMAGE_TAG || curl -fL# -o $BASE_IMAGE_TAG $BASE_IMAGE_LOC
 for img in "${@}"; do
     qemu-img create -b $BASE_IMAGE_TAG -f qcow2 -F qcow2 "${img}.img" 10G
     echo -e "instance-id: ${img}\nlocal-hostname: ${img}" > meta-data
-    genisoimage -output cidata.iso -V cidata -r -J user-data meta-data
+    genisoimage -output "${img}-cidata.iso" -V cidata -r -J user-data meta-data
     virt-install \
         --name="${img}" \
         --ram=2048 \
         --vcpus=2 \
         --import \
         --disk path="${img}.img",format=qcow2 \
-        --disk path=cidata.iso,device=cdrom \
+        --disk path="${img}-cidata.iso",device=cdrom \
         --os-variant=ubuntu20.04 \
         --network bridge=virbr0,model=virtio \
         --graphics vnc,listen=0.0.0.0 \
--- destroy-vm.sh
+++ destroy-vm.sh
@@ -4,8 +4,8 @@ for img in "${@}"; do
     virsh destroy --domain "${img}"
     virsh undefine --domain "${img}"
     rm -rf "${img}.img"
+    rm -rf "${img}-cidata.iso"
 done
 virsh pool-destroy --pool $(basename $PWD)
 virsh pool-undefine --pool $(basename $PWD)
-rm -rf cidata.iso
 rm -rf meta-data
